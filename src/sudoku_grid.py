44b9c659f53f0d0fdc091f33db3f450d2e69a393
26blevine <26blevine@gannacademy.org>
2025-08-30 13:10:06 -0400

    first try worked lets goo

--- src/sudoku_grid.py
+++ src/sudoku_grid.py
@@ -1,5 +1,61 @@
 def sudoku_grid_correct(sudoku: list):
-    pass
+    for row in range(len(sudoku)):
+        if row_correct(sudoku, row) == False:
+            return False
+    for col in range(len(sudoku[0])):
+        if column_correct(sudoku, col) == False:
+            return False
+    for row in range(0, len(sudoku), 3):
+        for col in range(0,len(sudoku[row]), 3):
+            if block_correct(sudoku, row, col) == False:
+                return False
+    return True
+
+
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    check = []
+    for row in range (row_no, row_no+3):
+        for col in range (column_no, column_no+3):
+            check.append(sudoku[row][col])
+    for val in range(1, 10):
+            seen_once = False
+            for num in check:
+                if num == val:
+                    if not seen_once:
+                        seen_once = True
+                    else:
+                        return False
+    return True
+
+def column_correct(list : list, column_num : int):
+    check = []
+    for row in list:
+        check.append(row[column_num])
+
+    for val in range(1, 10):
+        seen_once = False
+        for num in check:
+            if num == val:
+                if not seen_once:
+                    seen_once = True
+                else:
+                    return False
+    return True
+
+def row_correct(sudoku : list, row_index : int):
+    
+    check = sudoku[row_index]
+    for val in range(1, 10):
+        seen_once = False
+        for num in check:
+            if num == val:
+                if not seen_once:
+                    seen_once = True
+                else:
+                    return False
+    return True
+
+
 
 if __name__ == "__main__":
 
